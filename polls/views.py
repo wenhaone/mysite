b77806f734fc9119f2d4afedf530517573edb417
wenhao <18612398924@163.com>
2020-03-20 19:03:34 +0800

    在公司优化了通用视图、类视图

--- polls/views.py
+++ polls/views.py
@@ -3,8 +3,9 @@ from  django.template import loader
 from .models import Question,Choice
 from  django.shortcuts import render,get_object_or_404
 from  django.urls import  reverse
+from  django.views import generic
 
-def index(requset):
+def IndexView(requset):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
@@ -27,18 +28,25 @@ def detail(request,question_id):
     # return render(request,'polls/detail.html',{'question':question_id})
 #    return HttpResponse("You're looking at question %s."% question_id)
 
-def results(request,question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
-def vote(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
     try:
         selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError,Choice.DoesNotExist):
-        return render(request,'polls/detail.html',{'question':question,'error_message':"You didn't select a choice",})
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
     else:
-        selected_choice.votes +=1
+        selected_choice.votes += 1
         selected_choice.save()
-
-        return HttpResponseRedirect(reverse('polls.results',args=(question_id,)))
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        #重定向 ：HttpResponseRedirect   reverse：反解析
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
