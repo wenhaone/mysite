961bc3a13454985981839e9934ffc634978c9edb
wenhaohome <857449020@qq.com>
2020-03-20 23:28:24 +0800

    优化了通用试图

--- polls/urls.py
+++ polls/urls.py
@@ -7,9 +7,9 @@ app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
 
-    path('<int:question_id>/results', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/results', views.ResultsView.as_view(), name='results'),
 
     path('<int:question_id>/vote', views.vote, name='vote'),
 
--- polls/views.py
+++ polls/views.py
@@ -5,21 +5,39 @@ from  django.shortcuts import render,get_object_or_404
 from  django.urls import  reverse
 from  django.views import generic
 
-def IndexView(requset):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list':latest_question_list
-    }
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+    # latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list':latest_question_list
+    # }
+    #
 #    return  HttpResponse(template.render(context,requset))
 #升级后
-    return render(requset,'polls/index.html',context)
+    # return render(requset,'polls/index.html',context)
 #    output = ','.join([q.question_text for q in latest_question_list])
  #   return  HttpResponse(output)
 
-def detail(request,question_id):
-    question = get_object_or_404(Question,pk=question_id)
-    return  render(request,'polls/detail.html',{'question':question})
+# def detail(request,question_id):
+#     question = get_object_or_404(Question,pk=question_id)
+#     return  render(request,'polls/detail.html',{'question':question})
     # try:
     #     question = Question.objects.get(pk=question_id)
     # except Question.DoesNotExist:
@@ -28,9 +46,10 @@ def detail(request,question_id):
     # return render(request,'polls/detail.html',{'question':question_id})
 #    return HttpResponse("You're looking at question %s."% question_id)
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
